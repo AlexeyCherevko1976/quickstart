5d393680212e5ebf5002703758cea9a5060880c7
Alexey Cherevko <you@example.com>
2020-06-08 20:52:05 +0700

    %706 20-51 0806

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return view('tasks');
+});
+
+/** app/Http/routes.php
+   * Добавить новую задачу 
+   */
+  Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+  	]);
+
+	  if ($validator->fails()) {
+	    return redirect('/')
+	      ->withInput()
+	      ->withErrors($validator);
+	  }
+
+  // Создание задачи...
+  });
+
+  /** app/Http/routes.php
+   * Удалить задачу
+   */
+  Route::delete('/task/{task}', function (Task $task) {
+    //
+  });
